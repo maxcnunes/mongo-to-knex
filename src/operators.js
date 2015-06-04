9f62c42bdc91be6f20b38b18c5b650134f60d63f
Max Claus Nunes <maxcnunes@gmail.com>
2015-06-04 11:10:25 -0300

    adds docs about equal operator

--- src/operators.js
+++ src/operators.js
@@ -1,5 +1,7 @@
 /**
  * maps mongo query operators to sql operators
+ *
+ * query with no operator is mapped to equal operator (e.g. "{ name: 'jack'}" => "name = 'jack'")
  */
 export default {
   $lt: '<',
