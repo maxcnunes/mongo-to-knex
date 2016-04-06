49c7bad1f0c0f1acecd4836c81b5df23d399623c
Max Claus Nunes <maxcnunes@gmail.com>
2016-04-06 09:26:16 -0300

    Test the Knex output instead of through the Knex stub

--- spec/mongo-to-knex.spec.js
+++ spec/mongo-to-knex.spec.js
@@ -1,36 +1,34 @@
 import { expect } from 'chai';
-import sinon from 'sinon';
 import mongoToKnex from '../src';
 
 describe('mongo-to-knex', function () {
   beforeEach(function () {
     const knex = require('knex')({ dialect: 'sqlite3' });
     this.knexQuery = knex('table');
-    sinon.stub(this.knexQuery, 'where').returns(this.knexQuery);
   });
 
   it('should apply a query with no operator (equal)', function () {
     mongoToKnex({ age: 10 }, this.knexQuery);
-    expect(this.knexQuery.where.args[0]).to.eql([ 'age', '=', 10 ]);
+    expect(this.knexQuery.toString()).to.eql('select * from "table" where "age" = 10');
   });
 
   it('should apply a query with "$lt" operator', function () {
     mongoToKnex({ age: { $lt: 10 }}, this.knexQuery);
-    expect(this.knexQuery.where.args[0]).to.eql([ 'age', '<', 10 ]);
+    expect(this.knexQuery.toString()).to.eql('select * from "table" where "age" < 10');
   });
 
   it('should apply a query with "$lte" operator', function () {
     mongoToKnex({ age: { $lte: 10 }}, this.knexQuery);
-    expect(this.knexQuery.where.args[0]).to.eql([ 'age', '<=', 10 ]);
+    expect(this.knexQuery.toString()).to.eql('select * from "table" where "age" <= 10');
   });
 
   it('should apply a query with "$gt" operator', function () {
     mongoToKnex({ age: { $gt: 10 }}, this.knexQuery);
-    expect(this.knexQuery.where.args[0]).to.eql([ 'age', '>', 10 ]);
+    expect(this.knexQuery.toString()).to.eql('select * from "table" where "age" > 10');
   });
 
   it('should apply a query with "$gte" operator', function () {
     mongoToKnex({ age: { $gte: 10 }}, this.knexQuery);
-    expect(this.knexQuery.where.args[0]).to.eql([ 'age', '>=', 10 ]);
+    expect(this.knexQuery.toString()).to.eql('select * from "table" where "age" >= 10');
   });
 });
